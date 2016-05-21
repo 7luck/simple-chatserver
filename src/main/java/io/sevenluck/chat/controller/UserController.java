224fd758a2fa62b7f2d42ffb9224340455d32061
lukas <loki@linux-e0xq.Speedport_W_921V_1_39_000>
2016-05-21 15:01:02 +0200

    - exception handling

--- src/main/java/io/sevenluck/chat/controller/UserController.java
+++ src/main/java/io/sevenluck/chat/controller/UserController.java
@@ -11,39 +11,51 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author loki
  */
-
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    
+
     @Autowired
     private UserRepository userRepository;
-    
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    
-    @RequestMapping(method=RequestMethod.GET)
+
+    @RequestMapping(method = RequestMethod.GET)
     public List<User> getUsers() {
         return userRepository.findAll();
     }
-    
-    @RequestMapping(method=RequestMethod.POST)
+
+    @RequestMapping(value="{id}", method = RequestMethod.GET)
+    User getById(@PathVariable("id") Long id) {
+        return userRepository.findOne(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
     public User save(@RequestBody User user) {
         logger.info("{}", user);
-        
+
         return userRepository.save(user);
     }
     
-    
-    
-    
-    
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<?> handleException(Exception ex) {
+        return new ResponseEntity<>(new User("b","a"), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
